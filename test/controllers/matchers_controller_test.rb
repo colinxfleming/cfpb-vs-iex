d60bdb9949b0542afcd814497914cb0f99ddc839
colinxfleming <c3flemin@gmail.com>
2017-12-31 16:00:02 -0500

    whee

--- test/controllers/matchers_controller_test.rb
+++ test/controllers/matchers_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class MatchersControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get matchers_index_url
+    get matchers_path
     assert_response :success
   end
 
