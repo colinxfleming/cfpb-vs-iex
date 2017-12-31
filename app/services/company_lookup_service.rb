b622d6e84650ecbf61c54e24efce04aa2b6ba6af
colinxfleming <c3flemin@gmail.com>
2017-12-31 01:39:33 -0500

    lookup service should be its own instance object

--- app/services/company_lookup_service.rb
+++ app/services/company_lookup_service.rb
@@ -1,22 +1,26 @@
 # frozen_string_literal: true
 
 class CompanyLookupService
-  # CFPB_OPTIONS = load_cfpb_companies
-  # IEX_OPTIONS = load_iex_companies
-  # TODO Port this to run as an instance.
-
   NONWORDS = %w(inc the and)
 
+  attr_reader :cfpb_data, :iex_data
+  attr_accessor :cfpb_name, :iex_name
+
+  def initialize
+    @cfpb_data = load_cfpb_companies!
+    @iex_data = load_iex_companies!
+  end
+
   # Frontend access point. Look up general thing and confirm existence
-  def self.lookup(company)
-    cfpb_name = fuzzy_cfpb_lookup(company).first
+  def lookup(company)
+    @cfpb_name = fuzzy_cfpb_lookup(company).first
 
     iex_name = match_cfpb_to_iex cfpb_name
     { cfpb: cfpb_name['name'], iex: iex_name['name'] }
   end
 
   # Frontend access point. Pull down and return both
-  def self.pull_company(cfpb, iex)
+  def pull_company(cfpb, iex)
     # Start with CFPB because it is way more brittle
     complaint = CFPBService.search_complaints cfpb
     stock = IEXService.search_stocks iex
@@ -28,42 +32,44 @@ class CompanyLookupService
     }
   end
 
-  def self.fuzzy_cfpb_lookup(company)
-    load_cfpb_companies.select { |opt| /#{company}/i =~ opt['name'] }
+  private
+
+  def fuzzy_cfpb_lookup(company)
+    @cfpb_data.select { |opt| /#{company}/i =~ opt['name'] }
   end
 
-  def self.match_cfpb_to_iex(cfpb_name)
-    iex_data = load_iex_companies
+  def match_cfpb_to_iex(cfpb_name)
     core_name = clean cfpb_name['name']
 
-    exact_match = iex_data.find { |x| clean(x['name']) == core_name }
+    exact_match = @iex_data.find { |x| clean(x['name']) == core_name }
 
     if exact_match
       exact_match
     else
       # Sloppy rough matcher, but works for a proof of concept.
+      # Should do some word bucketing here instead.
+      # Note an
       iex_data.find { |x| clean(x['name'])[0] == core_name[0] }
     end
   end
 
-  def self.clean(company_name)
+  def clean(company_name)
     company_name.delete(',.')
                 .split(' ')
                 .reject { |word| NONWORDS.include? word.downcase }
                 .map(&:downcase)
   end
 
-  def self.rough_match(cfpb, iex)
-    /#{iex}/i =~ cfpb
+  def load_cfpb_companies!
+    load_dataset 'cfpb_companies'
   end
 
-  def self.load_cfpb_companies
-    filepath = File.join(Rails.root, 'app', 'datasets', 'cfpb_companies.json')
-    JSON.parse File.read(filepath)
+  def load_iex_companies!
+    load_dataset 'iex_companies'
   end
 
-  def self.load_iex_companies
-    filepath = File.join(Rails.root, 'app', 'datasets', 'iex_companies.json')
+  def load_dataset(dataset)
+    filepath = File.join(Rails.root, 'app', 'datasets', "#{dataset}.json")
     JSON.parse File.read(filepath)
   end
 end
