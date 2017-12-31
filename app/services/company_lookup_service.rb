ab68edcef29d75499222512a15cb423e6a6a27b9
colinxfleming <c3flemin@gmail.com>
2017-12-30 23:04:34 -0500

    stash some junk for qt. I think this clocks us at 2.5 hours

--- app/services/company_lookup_service.rb
+++ app/services/company_lookup_service.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 class CompanyLookupService
-  COMPANY_OPTIONS = load_cfpb_companies
+  # CFPB_OPTIONS = load_cfpb_companies
+  # IEX_OPTIONS = load_iex_companies
+  # Is this a good use case for a class variable? WOOF
+
+  NONWORDS = %w(inc the and)
 
   def self.lookup(company)
     cfpb_name = fuzzy_cfpb_lookup(company)
+
+    match_cfpb_to_iex cfpb_name
     # Do something to narrow down results
   end
 
@@ -15,7 +21,7 @@ class CompanyLookupService
 
   def self.fuzzy_cfpb_lookup(company)
     # Regex match 
-    COMPANY_OPTIONS.select { |opt| /#{company}/i =~ opt }
+    load_cfpb_companies.select { |opt| /#{company}/i =~ opt }
   end
 
   def self.load_cfpb_companies
@@ -24,5 +30,23 @@ class CompanyLookupService
         .split("\n") # ewwww
   end
 
-  # def self.load_
+  def self.load_iex_companies
+    filepath = File.join(Rails.root, 'app', 'datasets', 'iex_companies.json')
+    JSON.parse File.read(filepath)
+  end
+
+  def self.match_cfpb_to_iex(cfpb_name)
+    iex_data = load_iex_companies
+    core_name = cfpb_name.delete('.,')
+                         .split(' ')
+                         .reject { |word| NONWORDS.include? word.downcase }
+
+
+  end
+
+
+
+  def self.rough_match(cfpb, iex)
+    /#{iex}/i =~ cfpb
+  end
 end
