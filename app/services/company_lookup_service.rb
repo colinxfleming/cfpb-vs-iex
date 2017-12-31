7050817b78eb4fe40b3bd0036f1a7a3cd838e075
colinxfleming <c3flemin@gmail.com>
2017-12-30 22:32:08 -0500

    stash work -- looks like a web lookup + pull actual company is probably the way to go here

--- app/services/company_lookup_service.rb
+++ app/services/company_lookup_service.rb
@@ -1,15 +1,28 @@
 # frozen_string_literal: true
 
 class CompanyLookupService
+  COMPANY_OPTIONS = load_cfpb_companies
+
   def self.lookup(company)
-    # Start with CFPB because it is way more brittle
-    complaint = CFPBService.search_complaints company
-    
-
-
-
+    cfpb_name = fuzzy_cfpb_lookup(company)
+    # Do something to narrow down results
   end
 
+  def self.pull_company(company)
+    # Start with CFPB because it is way more brittle
+    complaint = CFPBService.search_complaints 
+  end
 
+  def self.fuzzy_cfpb_lookup(company)
+    # Regex match 
+    COMPANY_OPTIONS.select { |opt| /#{company}/i =~ opt }
+  end
 
+  def self.load_cfpb_companies
+    filepath = File.join(Rails.root, 'app', 'datasets', 'cfpb_companies.txt')
+    File.read(filepath)
+        .split("\n") # ewwww
+  end
+
+  # def self.load_
 end
