860f518bfff60064f1907881ae59d5d12f21c375
colinxfleming <c3flemin@gmail.com>
2017-12-30 21:23:22 -0500

    add service and test file

---
+++ app/services/iex_service.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class IEXService
+  BASE_URL = 'https://api.iextrading.com/1.0'.freeze
+  COMPANY_SYMBOLS_ENDPOINT = 'ref-data/symbols'.freeze
+
+  def self.search_stocks(company_name)
+    search_url = "#{BASE_URL}/#{stocks(company_name)}"
+
+    stock = HTTParty.get search_url
+    stock.parsed_response
+  end
+
+  def self.stocks(company_name)
+    company_symbol = company_symbols.find { |company| company['name'] == company_name }['symbol']
+    "stock/#{company_symbol.downcase}/quote"
+  end
+
+  def self.company_symbols
+    # Calling this every single time is silly, note to self to load it into a JSON CONST maybe
+    symbols_url = "#{BASE_URL}/#{COMPANY_SYMBOLS_ENDPOINT}"
+    HTTParty.get(symbols_url).parsed_response
+  end
+end
