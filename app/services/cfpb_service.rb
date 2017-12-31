c0895429823514d99177c7931ef5bcdc8eea4c1a
colinxfleming <c3flemin@gmail.com>
2017-12-30 20:42:48 -0500

    quick freeze

--- app/services/cfpb_service.rb
+++ app/services/cfpb_service.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class CFPBService
-  DATASET_IDENTIFIER = 'jhzv-w97w'
-  BASE_URL = 'https://data.consumerfinance.gov/resource'
-  DATASET_URL = "#{BASE_URL}/#{DATASET_IDENTIFIER}.json"
+  DATASET_IDENTIFIER = 'jhzv-w97w'.freeze
+  BASE_URL = 'https://data.consumerfinance.gov/resource'.freeze
+  DATASET_URL = "#{BASE_URL}/#{DATASET_IDENTIFIER}.json".freeze
 
   def self.search_complaints(company_name)
     # Socrata has a SQL-ish API. Nice of 'em
